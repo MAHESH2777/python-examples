413345ce9f1f2a4b83208f6ddff7f2077dea9bcd
Mahesh Kokkiripati <43738394+MAHESH2777@users.noreply.github.com>
2019-11-21 11:04:23 +0530

    few examples added

---
+++ join-method.py
@@ -0,0 +1,19 @@
+class custom(thread):
+     res=0
+    def run(self)
+        Print("calc starts...")
+        for i in range (1,default,n+1):
+            Custom.res=custom.res+i
+        print("calc ends....")
+        return
+class default:
+     n=0
+def main():
+    print("sun of first N number")
+    default.n=int(input ("Enter n val:"))
+    obj=custam()
+    obj.start()
+    obj.join()
+    print("sun:str(custam.res))
+    return
+default.main()    
